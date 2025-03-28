553a53dfff40fe6572e8dfc7b1892ff2af17445a
lior aharon <liorahron11@gmail.com>
2025-03-29 02:20:11 +0300

    Refactor tab layout and add SOS button functionality

---
+++ epi-com-client/app/(tabs)/sos.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import {Heading} from "@/components/ui/heading";
+import {Center} from "@/components/ui/center";
+
+export default function SOSScreen() {
+    return (
+        <View>
+            <Center className="p-20">
+                <Heading>SOS Page</Heading>
+            </Center>
+        </View>
+    );
+}
