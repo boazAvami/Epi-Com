0e7603dd3db2ee0bca1c38530760a5ec5a71035d
lior aharon <liorahron11@gmail.com>
2025-04-13 12:31:53 +0300

    refactor login screen; extract login card component, enhance loading state management, and improve user experience with loading messages

--- epi-com-client/app/(auth)/login.tsx
+++ epi-com-client/app/(auth)/login.tsx
@@ -1,215 +1,101 @@
-import {Router, useRouter} from 'expo-router';
-import {Keyboard, ScrollView} from 'react-native';
-import { useAuth } from '@/context/authContext';
-import {Button, ButtonIcon, ButtonText} from "@/components/ui/button"
-import {VStack} from "@/components/ui/vstack";
-import {Heading} from "@/components/ui/heading";
-import {
-    FormControl,
-    FormControlError,
-    FormControlErrorIcon, FormControlErrorText,
-} from "@/components/ui/form-control";
-import {Input, InputField, InputIcon, InputSlot} from "@/components/ui/input";
-import {Link, LinkText} from "@/components/ui/link";
-import {HStack} from "@/components/ui/hstack";
-import {AlertTriangle, EyeIcon, EyeOffIcon} from "lucide-react-native";
-import {Text} from "@/components/ui/text";
-import {Controller, useForm} from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {useState} from "react";
+import React, { useRef, useState, useEffect } from 'react';
+import { ScrollView } from 'react-native';
+import { Center } from '@/components/ui/center';
+import { Text } from '@/components/ui/text';
+import LoginCard, { LoginCardHandle } from "@/app/(auth)/login-card";
+import { HStack } from "@/components/ui/hstack";
+import { Link, LinkText } from "@/components/ui/link";
+import { Router, useRouter } from "expo-router";
+import { VStack } from "@/components/ui/vstack";
+import { Button, ButtonIcon, ButtonText } from "@/components/ui/button";
 import GoogleIcon from "@/assets/icons/google-icon";
-import {Center} from "@/components/ui/center";
-import {loginSchema, LoginSchemaType} from "@/schemas/login-schema";
+import LottieView from 'lottie-react-native';
 
-export default function LoginScreen() {
-    const { login, getUserInfo } = useAuth();
+const loadingMessages = [
+    "בודקים את הפרטים שהזנת...",
+    "תכף תוכל לראות מי יכול לעזור בקרבתך...",
+    "ממש תכף בפנים...",
+];
+
+const Login = () => {
     const router: Router = useRouter();
-    const handleLogin = (email: string, password: string) => {
-        login(email, password).then(() => {
-            getUserInfo().then(() => {
-                router.replace('/');
-            }).catch((err) => {
-                console.log(err);
-            });
-        }).catch((err) => {
-            if (err === 'wrong email or password') {
-                setValidated({ ...validated, emailValid: false });
-            }
-        })
+    const loginCardRef = useRef<LoginCardHandle>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [loadingTextIndex, setLoadingTextIndex] = useState(0);
+
+    const submit = () => {
+        loginCardRef.current?.submit();
     };
 
-    const {
-        control,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm<LoginSchemaType>({
-        resolver: zodResolver(loginSchema),
-    });
-    const [validated, setValidated] = useState({
-        emailValid: true,
-        passwordValid: true,
-    });
+    useEffect(() => {
+        if (!isLoading) return;
 
-    const onSubmit = (data: LoginSchemaType) => {
-        setValidated({ emailValid: true, passwordValid: true });
-        reset();
-        handleLogin(data.email, data.password);
-    };
-    const [showPassword, setShowPassword] = useState(false);
+        const interval = setInterval(() => {
+            setLoadingTextIndex((prev) => (prev + 1) % loadingMessages.length);
+        }, 2000);
 
-    const handleState = () => {
-        setShowPassword((showState) => {
-            return !showState;
-        });
-    };
-    const handleKeyPress = () => {
-        Keyboard.dismiss();
-        handleSubmit(onSubmit)();
-    };
-
-
-    const LoginCard = () => {
-        return (
-            <VStack className="max-w-[440px] w-full" space="3xl">
-                <VStack className="md:items-center" space="md">
-                        <Heading className="md:text-center text-right font-semibold text-[#333333]" size="2xl">
-                            ברוך הבא 👋
-                        </Heading>
-                    <Text className="md:text-center text-right text-[#4F4F4F]">התחבר כדי לשמור על עצמך ועל אחרים.</Text>
-                </VStack>
-                <VStack className="w-full mt-10">
-                    <VStack space="xl" className="w-full">
-                        <FormControl
-                            isInvalid={!!errors?.email || !validated.emailValid}
-                            className="w-full">
-                            <Controller
-                                defaultValue=""
-                                name="email"
-                                control={control}
-                                rules={{
-                                    validate: async (value) => {
-                                        try {
-                                            await loginSchema.parseAsync({ email: value });
-                                            return true;
-                                        } catch (error: any) {
-                                            return error.message;
-                                        }
-                                    },
-                                }}
-                                render={({ field: { onChange, onBlur, value } }) => (
-                                    <Input>
-                                        <InputField
-                                            className="text-right"
-                                            placeholder="כתובת מייל"
-                                            value={value}
-                                            onChangeText={onChange}
-                                            onBlur={onBlur}
-                                            onSubmitEditing={handleKeyPress}
-                                            returnKeyType="done"
-                                        />
-                                    </Input>
-                                )}
-                            />
-                            <FormControlError className="justify-end">
-                                <FormControlErrorText>
-                                    {errors?.email?.message ||
-                                        (!validated.emailValid && "כתובת מייל או סיסמה לא נמצאו")}
-                                </FormControlErrorText>
-                                <FormControlErrorIcon as={AlertTriangle} />
-                            </FormControlError>
-                        </FormControl>
-                        {/* Label Message */}
-                        <FormControl
-                            isInvalid={!!errors.password || !validated.passwordValid}
-                            className="w-full"
-                        >
-                            <Controller
-                                defaultValue=""
-                                name="password"
-                                control={control}
-                                rules={{
-                                    validate: async (value) => {
-                                        try {
-                                            await loginSchema.parseAsync({ password: value });
-                                            return true;
-                                        } catch (error: any) {
-                                            return error.message;
-                                        }
-                                    },
-                                }}
-                                render={({ field: { onChange, onBlur, value } }) => (
-                                    <Input>
-                                        <InputSlot onPress={handleState} className="p-2">
-                                            <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
-                                        </InputSlot>
-                                        <InputField
-                                            className="text-right"
-                                            type={showPassword ? "text" : "password"}
-                                            placeholder="סיסמה"
-                                            value={value}
-                                            onChangeText={onChange}
-                                            onBlur={onBlur}
-                                            onSubmitEditing={handleKeyPress}
-                                            returnKeyType="done"
-                                        />
-                                    </Input>
-                                )}
-                            />
-                            <FormControlError className="justify-end">
-                                <FormControlErrorText>
-                                    {errors?.password?.message ||
-                                        (!validated.passwordValid && "סיסמה לא נכונה")}
-                                </FormControlErrorText>
-                                <FormControlErrorIcon as={AlertTriangle} />
-
-                            </FormControlError>
-                        </FormControl>
-                    </VStack>
-                    <VStack className="w-full my-7 " space="lg">
-                        <Button
-                            className="w-full"
-                            onPress={handleSubmit(onSubmit)}
-                            style={{backgroundColor: '#FE385C', borderRadius: 20}}>
-                            <ButtonText className="font-medium">התחבר</ButtonText>
-                        </Button>
-                        <Button
-                            style={{borderRadius: 20}}
-                            variant="outline"
-                            action="secondary"
-                            className="w-full gap-1"
-                            onPress={() => {}}>
-
-                            <ButtonText className="font-medium">
-                                כניסה דרך גוגל
-                            </ButtonText>
-                            <ButtonIcon as={GoogleIcon} className="text-typography-white"/>
-                        </Button>
-                    </VStack>
-                    <HStack className="self-center" space="sm">
-                        <Link onPress={() => router.push('/register-intro')}>
-                            <LinkText
-                                className="font-medium text-primary-700 group-hover/link:text-primary-600  group-hover/pressed:text-primary-700"
-                                size="md"
-                            >
-                                להרשמה
-                            </LinkText>
-                        </Link>
-                        <Text size="md">אין לך חשבון?</Text>
-                    </HStack>
-                </VStack>
-            </VStack>
-        );
-    };
+        return () => clearInterval(interval);
+    }, [isLoading]);
 
     return (
-        <ScrollView className="p-16" contentContainerStyle={{
-            flexGrow: 1,
-            justifyContent: 'center',
-        }}>
-            <Center className="h-full ">
-                <LoginCard/>
+        <ScrollView
+            className="p-16"
+            contentContainerStyle={{
+                flexGrow: 1,
+                justifyContent: 'center',
+            }}
+        >
+            <Center className="h-full">
+                {isLoading ? (
+                    <VStack className="items-center space-y-4">
+                        <LottieView
+                            source={require('@/assets/animations/register-loading.json')}
+                            autoPlay
+                            loop
+                            style={{ width: 200, height: 200 }}
+                        />
+                        <Text className="text-lg text-center text-[#4F4F4F]">
+                            {loadingMessages[loadingTextIndex]}
+                        </Text>
+                    </VStack>
+                ) : (
+                    <VStack className="max-w-[440px] w-[80%]">
+                        <LoginCard ref={loginCardRef} setIsLoading={setIsLoading} />
+                        <VStack className="w-full my-7" space="lg">
+                            <Button
+                                className="w-full"
+                                onPress={submit}
+                                style={{ backgroundColor: '#FE385C', borderRadius: 20 }}
+                            >
+                                <ButtonText className="font-medium">התחבר</ButtonText>
+                            </Button>
+                            <Button
+                                style={{ borderRadius: 20 }}
+                                variant="outline"
+                                action="secondary"
+                                className="w-full gap-1"
+                                onPress={() => {}}
+                            >
+                                <ButtonText className="font-medium">כניסה דרך גוגל</ButtonText>
+                                <ButtonIcon as={GoogleIcon} className="text-typography-white" />
+                            </Button>
+                        </VStack>
+                        <HStack className="self-center" space="sm">
+                            <Link onPress={() => router.push('/register-intro')}>
+                                <LinkText
+                                    className="font-medium text-primary-700"
+                                    size="md"
+                                >
+                                    להרשמה
+                                </LinkText>
+                            </Link>
+                            <Text size="md">אין לך חשבון?</Text>
+                        </HStack>
+                    </VStack>
+                )}
             </Center>
         </ScrollView>
     );
-}
+};
+
+export default Login;
